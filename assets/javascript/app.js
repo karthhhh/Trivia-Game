b4a6893bfce6675517f400faf88ccfd672bd9191
Karthhhh <karth423@gmail.com>
2017-02-15 17:26:34 -0800

    Trivia-Game

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+ var questions = [
+        // question 1
+        {
+            "q": "Who is the only Quarterback with 5 superbowl rings?",
+            "c": ["Tom Brady", "Joe Montana", "Terry Bradshaw"],
+            "answer": 0
+        },
+        // question 2
+        {
+            "q": "What is the only NFL team to have a perfect regular season record in the Superbowl Era?",
+            "c": ["Patriots", "Dolphins", "Colts"],
+            "answer": 0
+        },
+        // question 3
+        {
+            "q": "What is the most successful Quarterbacks name?",
+            "c": ["Tom Brady", "Bart Starr", "Otto Graham"],
+            "answer": 2
+        },
+        // question 4
+        {
+            "q": "Which 2 quarterbacks had the best rivallry of the Superbowl Era?",
+            "c": ["Brady and Manning", "Rothlisberger and Flacco", "Rivers and Brees"],
+            "answer": 0
+        },
+        // question 5
+        {
+            "q": "Who has the most wins as Head Coach?",
+            "c": ["Bill Belichek", "Don Shula", "Tom Landry"],
+            "answer": 1
+        },
+        // question 6
+        {
+            "q": "What NFL team only has a decal on one side of their helmets?",
+            "c": ["Steelers", "Cowboys", "Titans"],
+            "answer": 1
+        },
+        // question 7
+        {
+            "q": "Who is the last non-quarterback to win NFL MVP?",
+            "c": ["LaDanian Tomlinson", "Adrian Peterson", "Shawn Alexander"],
+            "answer": 1
+        },
+        // question 8
+        {
+            "q": "This current NFL quarterback, a 2010 Pro Bowler, never started a game at QB in college?",
+            "c": ["Matt Cassel", "Brian Hoyer", "Matt Moore"],
+            "answer": 0
+        },
+        // question 9
+        {
+            "q": "How many Heisman Trophy winners have gone on to be MVP of the Super Bowl?",
+            "c": ["2", "4", "6"],
+            "answer": 1
+        },
+        // question 10
+        {
+            "q": "4 of the first 5 picks in the 1989 draft -- Troy Aikman, Barry Sanders, Derrick Thomas and Deion Sanders -- are in the Hall of Fame. Who was the bust?",
+            "c": ["Blair Thomas", "Tony Mandarich", "Kieth McCants"],
+            "answer": 1
+        }
+    ];
